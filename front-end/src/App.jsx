45fa07f5b4007b9a41055fb2739af74b0111e6a2
DANIEL ALMEIDA LOVAGLIO <daniel.lovaglio2@senaisp.edu.br>
2026-08-28 15:21:55 -0300

    :bricks:ci rotas

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+
 import Login from './pages/login.page.jsx';
 import Cadastro from './pages/cadastro.page.jsx';
 import Dashboard from './pages/dashboard.page.jsx';
@@ -9,15 +10,17 @@ import NovoAgendamento from './pages/novo_agendamento.page.jsx';
 import LoginMedico from './pages/loginMedico.jsx';
 
 export default function App() {
-  return <Routes>
-    <Route path="/" element={<Login />} />
-    <Route path="/cadastro" element={<Cadastro />} />
-    <Route path="/login-medico" element={<LoginMedico />} />
-    <Route path="/dashboard" element={<Dashboard />} />
-    <Route path="/perfil" element={<Perfil />} />
-    <Route path="/perfil/editar" element={<EditarPerfil />} />
-    <Route path="/agendamentos" element={<Agendamentos />} />
-    <Route path="/agendamentos/novo" element={<NovoAgendamento />} />
-    <Route path="*" element={<Navigate to="/" replace />} />
-  </Routes>;
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/cadastro" element={<Cadastro />} />
+      <Route path="/login-medico" element={<LoginMedico />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/perfil" element={<Perfil />} />
+      <Route path="/perfil/editar" element={<EditarPerfil />} />
+      <Route path="/agendamentos" element={<Agendamentos />} />
+      <Route path="/agendamentos/novo" element={<NovoAgendamento />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
 }
