7efa49a460af70650a997b529fcb33206dae2378
JÚLIA DE SOUZA BUSCARIOLLO <julia.buscariollo2@senaisp.edu.br>
2026-08-28 14:27:43 -0300

    :bug:fix corrigindo a pagina do medico

--- front-end/src/services/medicoService.js
+++ front-end/src/services/medicoService.js
@@ -1,11 +1,30 @@
 import { api } from './api';
 
 export async function loginMedico(dados) {
-  const response = await api.post('/login/medico', dados);
-  return response.data;
+    const response =
+        await api.post('/login/medico', dados);
+    return response.data;
 }
 
 export async function cadastrarMedico(dados) {
-  const response = await api.post('/medicos', dados);
-  return response.data;
+    const response =
+        await api.post('/medicos',dados);
+    return response.data;
+}
+
+export async function listarProcedimentosMedico(idMedico) {
+    const response =
+        await api.get(`/medicos/${idMedico}/procedimentos`);
+    return response.data;
+}
+
+export async function adicionarProcedimentoMedico(idMedico, idProcedimento) {
+    const response =
+        await api.post(`/medicos/${idMedico}/procedimentos`,
+            {
+                id_procedimento:
+                    idProcedimento
+            }
+        );
+    return response.data;
 }
