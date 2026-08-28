f471ed4c79908d853f10b0ede00cccf483a2072a
DANIEL ALMEIDA LOVAGLIO <daniel.lovaglio2@senaisp.edu.br>
2026-08-28 15:19:19 -0300

    :bricks:ci criado nova função para agendamento cliente

--- front-end/src/services/clienteService.js
+++ front-end/src/services/clienteService.js
@@ -1,9 +1,7 @@
 import { api } from './api.js';
 
 export async function buscarCliente(id) {
-  const response = await api.get(
-    `/clientes/${id}`
-  );
+  const response = await api.get(`/clientes/${id}`);
 
   return response.data;
 }
@@ -15,18 +13,13 @@ export async function listarClientes() {
 }
 
 export async function atualizarCliente(id, dados) {
-  const response = await api.put(
-    `/clientes/${id}`,
-    dados
-  );
+  const response = await api.put(`/clientes/${id}`, dados);
 
   return response.data;
 }
 
 export async function excluirCliente(id) {
-  const response = await api.delete(
-    `/clientes/${id}`
-  );
+  const response = await api.delete(`/clientes/${id}`);
 
   return response.data;
 }
