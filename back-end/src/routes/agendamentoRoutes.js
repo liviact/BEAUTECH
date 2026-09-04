a868f37bce5f0ac08d827aeafb572b123af23252
DANIEL ALMEIDA LOVAGLIO <daniel.lovaglio2@senaisp.edu.br>
2026-09-04 12:55:14 -0300

    :bug: atualizado as rotas

--- back-end/src/routes/agendamentoRoutes.js
+++ back-end/src/routes/agendamentoRoutes.js
@@ -7,12 +7,10 @@ const router = express.Router();
 router.post('/', authMiddleware, agendamentoController.criar);
 router.get('/', authMiddleware, agendamentoController.selecionar);
 router.get('/agenda-medico', authMiddleware, agendamentoController.agendaMedico);
-
 router.put('/:id/aceitar', authMiddleware, agendamentoController.aceitar);
 router.put('/:id/recusar', authMiddleware, agendamentoController.recusar);
 router.put('/:id/cancelar', authMiddleware, agendamentoController.cancelar);
 router.put('/:id/realizar', authMiddleware, agendamentoController.realizar);
-
 router.post('/:id/reagendar', authMiddleware, agendamentoController.reagendar);
 router.put('/:id', authMiddleware, agendamentoController.editar);
 
